a55fe2729c8af7aed9d57b566ab95a37ef9197ef
Younes202 <84544672+Younes202@users.noreply.github.com>
2022-07-11 15:54:44 +0200

    v-for (Loop)

---
+++ Vue_5.js
@@ -0,0 +1,11 @@
+var app5 = new Vue({
+    el:"#app_5",
+    data:{
+        b : [
+            {text : "Hello I'm the First paragraph"},
+            {text: "Hello I'm the Second paragraph"},
+            {text : "Hello I'm the third paragraph"}
+        ]
+    }
+
+})
